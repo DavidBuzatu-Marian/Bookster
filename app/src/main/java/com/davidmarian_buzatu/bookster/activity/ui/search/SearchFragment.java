05169ffee083506b431c4198f26048be1fb16a89
Caracoancea Timotei <caracoancea.timotei@gmail.com>
2020-04-30 21:49:52 +0300

    Initialized classes for SearchView BMA-33

--- app/src/main/java/com/davidmarian_buzatu/bookster/activity/ui/search/SearchFragment.java
+++ app/src/main/java/com/davidmarian_buzatu/bookster/activity/ui/search/SearchFragment.java
@@ -6,14 +6,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -29,6 +33,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
 
@@ -44,6 +49,7 @@ public class SearchFragment extends Fragment {
 
         getUserInstance(getArguments().getString("Type"));
         getUserInfo(root);
+        setUpSearchView(root);
         setUpCalendarPicker(root);
         setUpNumberPicker(root);
         setUpSubmitButton(root);
@@ -82,6 +88,48 @@ public class SearchFragment extends Fragment {
                 .append(getString(R.string.frag_search_TV_welcome_user))
                 .append(", ")
                 .append(mCurrentUser.getUserName()).toString());
+    }
+
+    private void setUpSearchView(View root){
+        SearchView searchView=root.findViewById(R.id.frag_search_SV);
+        ListView listView=root.findViewById(R.id.frag_search_LV);
+        ArrayList<String> list;
+        ArrayAdapter<String> adapter;
+
+        list=new ArrayList<>(3);
+        list.add("Rome");
+        list.add("Paris");
+        list.add("Timisoara");
+
+        adapter=new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(adapter);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                if(list.contains(query)){
+                    adapter.getFilter().filter(query);
+                }
+                else{
+                    Toast.makeText(getContext(),"No match found",Toast.LENGTH_LONG).show();
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if(list.contains(newText)){
+                    adapter.getFilter().filter(newText);
+                }
+                else{
+                    Toast.makeText(getContext(),"No match found",Toast.LENGTH_LONG).show();
+                }
+                return false;
+            }
+        });
+
     }
 
     private void getUserInstance(String type) {
