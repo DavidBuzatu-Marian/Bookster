ec78b892dbda0c86b140f48f1dfdd07a6f9770cf
davidbuzatu-marian <davidm.buzatu@gmail.com>
2020-05-01 13:10:39 +0300

    Implemented SearchView with functionality (BMA-33)

--- app/src/main/java/com/davidmarian_buzatu/bookster/activity/ui/search/SearchFragment.java
+++ app/src/main/java/com/davidmarian_buzatu/bookster/activity/ui/search/SearchFragment.java
@@ -4,27 +4,26 @@ import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.FrameLayout;
 import android.widget.NumberPicker;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
+import com.arlib.floatingsearchview.FloatingSearchView;
+import com.arlib.floatingsearchview.suggestions.model.SearchSuggestion;
 import com.davidmarian_buzatu.bookster.R;
-import com.davidmarian_buzatu.bookster.activity.MenuActivity;
-import com.davidmarian_buzatu.bookster.activity.ui.search.helper.UploadCities;
+import com.davidmarian_buzatu.bookster.activity.ui.search.helper.SearchList;
 import com.davidmarian_buzatu.bookster.model.Client;
 import com.davidmarian_buzatu.bookster.model.Manager;
 import com.davidmarian_buzatu.bookster.model.User;
@@ -35,6 +34,7 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
 public class SearchFragment extends Fragment {
@@ -90,47 +90,54 @@ public class SearchFragment extends Fragment {
                 .append(mCurrentUser.getUserName()).toString());
     }
 
-    private void setUpSearchView(View root){
-        SearchView searchView=root.findViewById(R.id.frag_search_SV);
-        ListView listView=root.findViewById(R.id.frag_search_LV);
-        ArrayList<String> list;
-        ArrayAdapter<String> adapter;
 
-        list=new ArrayList<>(3);
-        list.add("Rome");
-        list.add("Paris");
-        list.add("Timisoara");
+    private void setUpSearchView(View root) {
+        FloatingSearchView searchView = root.findViewById(R.id.frag_search_FSV);
 
-        adapter=new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,list);
-        listView.setAdapter(adapter);
+        List<SearchList> list = new ArrayList<>(3);
+        list.add(new SearchList("Rome"));
+        list.add(new SearchList("Paris"));
+        list.add(new SearchList("Timisoara"));
 
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        searchView.setOnQueryChangeListener(new FloatingSearchView.OnQueryChangeListener() {
             @Override
-            public boolean onQueryTextSubmit(String query) {
+            public void onSearchTextChanged(String oldQuery, String newQuery) {
+                if (!oldQuery.equals("") && newQuery.equals("")) {
+                    searchView.clearSuggestions();
+                } else {
 
-                if(list.contains(query)){
-                    adapter.getFilter().filter(query);
+                    searchView.swapSuggestions(
+                            getFilteredList(list, newQuery));
                 }
-                else{
-                    Toast.makeText(getContext(),"No match found",Toast.LENGTH_LONG).show();
-                }
+            }
+        });
+        searchView.setOnSearchListener(new FloatingSearchView.OnSearchListener() {
+            @Override
+            public void onSuggestionClicked(SearchSuggestion searchSuggestion) {
+                searchView.setSearchText(searchSuggestion.getBody());
+                searchView.clearSuggestions();
 
-                return false;
             }
 
             @Override
-            public boolean onQueryTextChange(String newText) {
-                if(list.contains(newText)){
-                    adapter.getFilter().filter(newText);
-                }
-                else{
-                    Toast.makeText(getContext(),"No match found",Toast.LENGTH_LONG).show();
-                }
-                return false;
+            public void onSearchAction(String currentQuery) {
+
             }
         });
 
     }
+
+    private List<? extends SearchSuggestion> getFilteredList(List<SearchList> list, String newQuery) {
+        List<SearchList> result = new ArrayList<>();
+        for (SearchList sl : list) {
+            // Transform string to lowercase for cases like 'rome' and 'Rome'
+            if (sl.getBody().toLowerCase().contains(newQuery.toLowerCase())) {
+                result.add(sl);
+            }
+        }
+        return result;
+    }
+
 
     private void getUserInstance(String type) {
         switch (type) {
