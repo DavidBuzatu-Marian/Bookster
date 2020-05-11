944b2bab4b1685ca53e3969d89b4452db274472e
davidbuzatu-marian <davidm.buzatu@gmail.com>
2020-05-11 19:50:10 +0300

    Finished XML for adding an offer (BMA-47)

--- app/src/main/java/com/davidmarian_buzatu/bookster/activity/ui/offers/AddOfferFragment.java
+++ app/src/main/java/com/davidmarian_buzatu/bookster/activity/ui/offers/AddOfferFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
@@ -24,14 +25,33 @@ import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.davidmarian_buzatu.bookster.R;
+import com.davidmarian_buzatu.bookster.activity.ui.search.helper.UploadCities;
 import com.davidmarian_buzatu.bookster.services.CalendarActions;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class AddOfferFragment extends Fragment {
     private static final int SELECT_PICTURES = 1001;
+    private List<Bitmap> mPicturesList;
+    private List<String> mPicturesLink;
     private CalendarActions mCalendarActions;
+    private View mRoot;
 
     @Nullable
     @Override
@@ -39,6 +59,7 @@ public class AddOfferFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_add_offer, container, false);
 
         setUpView(root);
+        mRoot = root;
         return root;
     }
 
@@ -48,6 +69,19 @@ public class AddOfferFragment extends Fragment {
         setUpCalendarPicker(root);
         setUpNumberPicker(root);
         setAddImagesButton(root);
+        setSpinner(root, R.id.frag_addOffer_SP_city, new UploadCities().getCities()); // set spinner for city
+        setSpinner(root, R.id.frag_addOffer_SP_country, new UploadCities().getCountries()); // set spinner for country
+    }
+
+    private void setSpinner(View root, int resId, String[] arrayOfVals) {
+        Spinner countrySpinner = root.findViewById(resId);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, arrayOfVals);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        countrySpinner.setAdapter(adapter);
+    }
+
+    private void setSpinnerCountry(View root) {
+
     }
 
     private void setAddImagesButton(View root) {
@@ -101,6 +135,34 @@ public class AddOfferFragment extends Fragment {
         }
     }
 
+    private void saveImagesToStorage() {
+        mPicturesList = new ArrayList<>();
+        for (Bitmap bitmap : mPicturesList) {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] data = baos.toByteArray();
+            StorageReference propertiesRef = FirebaseStorage.getInstance().getReference().child("properties_all/" + UUID.randomUUID());
+            UploadTask uploadTask = propertiesRef.putBytes(data);
+
+            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if (!task.isSuccessful()) {
+                        throw task.getException();
+                    }
+                    return propertiesRef.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()) {
+                        mPicturesLink.add(task.toString());
+                    }
+                }
+            });
+        }
+    }
+
     private void setSpinnerRoomType(View root) {
         Spinner spinner = root.findViewById(R.id.frag_addOffer_SP_room_type);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
@@ -114,22 +176,29 @@ public class AddOfferFragment extends Fragment {
         if (requestCode == SELECT_PICTURES) {
             if (resultCode == Activity.RESULT_OK) {
                 if (data.getClipData() != null) {
+                    mPicturesList = new ArrayList<>();
                     int count = data.getClipData().getItemCount();
-                    for (int i = 0; i < count; i++) {
-                        final Uri imageUri = data.getClipData().getItemAt(i).getUri();
-                        final InputStream imageStream;
-                        try {
-                            imageStream = getActivity().getContentResolver().openInputStream(imageUri);
-                            final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
-                            Log.d("TEST", selectedImage.toString());
-                        } catch (FileNotFoundException e) {
-                            e.printStackTrace();
-                        }
-
-                    }
+                    getPictures(data, count);
+                    Snackbar.make(mRoot, count + " images selected", BaseTransientBottomBar.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getContext(), "Please select at least 2 pictures", Toast.LENGTH_LONG).show();
                 }
             } else {
                 Toast.makeText(getContext(), "Could not select images", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void getPictures(Intent data, int count) {
+        for (int i = 0; i < count; i++) {
+            final Uri imageUri = data.getClipData().getItemAt(i).getUri();
+            final InputStream imageStream;
+            try {
+                imageStream = getActivity().getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                mPicturesList.add(selectedImage);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
             }
         }
     }
